993d0570ec55e0e55349ff8677bc819d47186c0b
John Doe <johndoe@example.com>
2023-01-12 11:15:38 +0100

    smycka

--- prvocislo.py
+++ prvocislo.py
@@ -1,8 +1,19 @@
 # Program to check if a number is prime or not
 
-import random
+#import random
 # num = 29
-num = random.randint(0, 100)
+#num = random.randint(0, 100)
+# input
+num = int(input())
+  
+print
+print
+print
+print
+print(num)
+# output
+
+
 
 # To take input from the user
 #num = int(input("Enter a number: "))
@@ -11,7 +22,7 @@ num = random.randint(0, 100)
 flag = False
 
 if num == 1:
-    print(num, "není prvočíslo")
+    print(num, "je prvočíslo")
 elif num > 1:
     # check for factors
     for i in range(2, num):
@@ -23,6 +34,7 @@ elif num > 1:
 
     # check if flag is True
     if flag:
-        print(num, "je prvočíslo")
-    else:
         print(num, "není prvočíslo")
+    else:
+        print(num, "je1
+         prvočíslo")
